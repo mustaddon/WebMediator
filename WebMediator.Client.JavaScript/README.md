420c3f195911803801ebb40e43adafebc2622145
Leonid Salavatov <mustaddon@gmail.com>
2026-04-12 17:01:35 +0300

    readme

--- WebMediator.Client.JavaScript/README.md
+++ WebMediator.Client.JavaScript/README.md
@@ -12,7 +12,7 @@ import { WebMediatorClient } from 'web-mediator-client';
 
 const client = new WebMediatorClient('https://localhost:7263/mediator');
 
-let response = await client.send('Ping', { Message: 'TEST' });
+const response = await client.send('Ping', { Message: 'TEST' });
 
 console.log(response.data);
 ```
@@ -31,9 +31,9 @@ import { WebMediatorClient } from 'web-mediator-client';
 const client = new WebMediatorClient('https://localhost:7263/mediator');
 
 
-let fileData = new Blob(["Hello, world!"], { type: "text/plain" });
+const fileData = new Blob(["Hello, world!"], { type: "text/plain" });
 // OR 
-// let fileData = document.getElementById('my-input').files[0];
+// fileData = document.getElementById('my-input').files[0];
 
 await client.send('FileUpload', { Name: 'example.txt', Content: fileData });
 
@@ -49,7 +49,7 @@ import { WebMediatorClient } from 'web-mediator-client';
 
 const client = new WebMediatorClient('https://localhost:7263/mediator');
 
-let asyncEvents = client.eventStream('ExampleAsyncEvents');
+const asyncEvents = client.eventStream('ExampleAsyncEvents');
 
 for await (const e of asyncEvents) { 
     console.log(e); 
--- WebMediator.Client.JavaScript/test.js
+++ WebMediator.Client.JavaScript/test.js
@@ -3,23 +3,23 @@ import { WebMediatorClient } from './index.js';
 const client = new WebMediatorClient('https://localhost:7263/mediator');
 
 
-let res0 = await client.send('Echo');
+const res0 = await client.send('Echo');
 console.log(res0);
 
-let res1 = await client.send('Ping', { Message: 'TEXT' });
+const res1 = await client.send('Ping', { Message: 'TEXT' });
 console.log(res1);
 
 await client.send('FileUpload', { Name: 'test.txt', Content: new Blob(['text text text'], { type: 'text/plain' }) });
 
-let res3 = await client.send('FileDownload', { Name: 'test.txt' });
+const res3 = await client.send('FileDownload', { Name: 'test.txt' });
 console.log(res3, await res3.data.text());
 
-let res4 = await client.send('FileDownloadWithInfo', { Name: 'test.txt' });
+const res4 = await client.send('FileDownloadWithInfo', { Name: 'test.txt' });
 console.log(res4, await res4.data.Content.text());
 
 
 
-let res5 = await client.send('ExampleAsyncEvents');
+const res5 = await client.send('ExampleAsyncEvents');
 console.log(res5);
 
 let i=0;
@@ -30,7 +30,7 @@ for await (const item of res5.data) {
 
 
 
-let asyncEvents = client.eventStream('ExampleAsyncEvents');
+const asyncEvents = client.eventStream('ExampleAsyncEvents');
 console.log(asyncEvents);
 
 i=0;
